0e61ee962b13f4c8067dddb4979021762f695e80
Juan Santos <juan.moreira62@outlook.com>
2024-10-28 20:11:58 -0300

    creating repo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,20 @@
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 
 accordionHeaders.forEach(header => {
-  header.addEventListener("click", () =>{
+  header.addEventListener("click", () => {
     const accordionitem = header.parentElement
     const accordionActive = accordionitem.classList.contains("active")
 
     accordionActive ? accordionitem.classList.remove("active") : accordionitem.classList.add("active")
+  })
+})
+
+
+const menuLinks = document.querySelectorAll(".menu__link")
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+menuLinks.forEach(i => i.classList.remove("active"))
+item.classList.add("active")
   })
 })
